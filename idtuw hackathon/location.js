02c0dd335eb72bc25615ffa3038800198c6b8af6
sanyayaa <103187781+sanyayaa@users.noreply.github.com>
2022-09-27 01:38:18 +0530

    Add files via upload

---
+++ idtuw hackathon/location.js
@@ -0,0 +1,20 @@
+navigator.geolocation.getCurrentPosition(
+    (data) => {
+        console.log(data.coords)
+
+        axios({
+            url: `https://api.opencagedata.com/geocode/v1/json?key=195b1555711d428f8c35e9d5ee9da5a0&q=${encodeURIComponent(data.coords.latitude + "," + data.coords.longitude)}&pretty=1&no_annotations=1`,
+            method: "GET"
+        })
+            .then((res) => {
+                console.log(res.data.results[0].formatted)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+    },
+    (error) => {
+        console.error(error)
+    }
+)
